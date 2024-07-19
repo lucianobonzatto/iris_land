0d7045b29bc2ec624d1574dc6744551abde03635
Luciano Bonzatto Junior <luciano.bj23@hotmail.com>
2024-07-18 22:34:43 -0300

    tab correction

--- iris_land/src/manager_node.cpp
+++ iris_land/src/manager_node.cpp
@@ -5,24 +5,25 @@
 
 int main(int argc, char *argv[])
 {
-  ros::init(argc, argv, "manager_control");
-  ros::NodeHandle *nh = new ros::NodeHandle();
-  ros::Rate loop_rate(20);
+    ros::init(argc, argv, "manager_control");
+    ros::NodeHandle *nh = new ros::NodeHandle();
+    ros::Rate loop_rate(20);
 
-  Manager manager;
-  DroneControl drone_control;
-  ROSClient ros_client(nh);
+    Manager manager;
+    DroneControl drone_control;
+    ROSClient ros_client(nh);
 
-  ros_client.Init(&manager, &drone_control);
-  manager.Init(&ros_client);
-  drone_control.Init(&ros_client);
+    ros_client.Init(&manager, &drone_control);
+    drone_control.Init(&ros_client);
+    manager.Init(&ros_client, &drone_control);
 
-  while(ros::ok()){
-    ros::spinOnce();
-    manager.print_parameters();
-    manager.update();
-    
-    loop_rate.sleep();
-  }
-  delete nh;
+    while (ros::ok())
+    {
+        ros::spinOnce();
+        manager.print_parameters();
+        manager.update();
+
+        loop_rate.sleep();
+    }
+    delete nh;
 }
