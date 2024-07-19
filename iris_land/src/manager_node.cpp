b4cf8a302effc0d1c9ce7e4b5e48b031d9d135a1
Luciano Bonzatto Junior <luciano.bj23@hotmail.com>
2024-07-18 22:31:52 -0300

    create DroneControl obj

--- iris_land/src/manager_node.cpp
+++ iris_land/src/manager_node.cpp
@@ -1,8 +1,7 @@
 #include "general.h"
 #include "manager.h"
 #include "ros_client.h"
-
-Manager principal;
+#include "drone_control.h"
 
 int main(int argc, char *argv[])
 {
@@ -10,13 +9,18 @@ int main(int argc, char *argv[])
   ros::NodeHandle *nh = new ros::NodeHandle();
   ros::Rate loop_rate(20);
 
+  Manager manager;
+  DroneControl drone_control;
   ROSClient ros_client(nh);
-  principal.Init(&ros_client);
+
+  ros_client.Init(&manager, &drone_control);
+  manager.Init(&ros_client);
+  drone_control.Init(&ros_client);
 
   while(ros::ok()){
     ros::spinOnce();
-    principal.print_parameters();
-    principal.update();
+    manager.print_parameters();
+    manager.update();
     
     loop_rate.sleep();
   }
