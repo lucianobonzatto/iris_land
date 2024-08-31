cc9a0e0327440a492523277e167727547ecdf7da
Luciano Bonzatto Junior <luciano.bj23@hotmail.com>
2024-08-31 14:34:33 -0300

    remove unused

--- iris_land/include/iris_land/manager.cpp
+++ iris_land/include/iris_land/manager.cpp
@@ -38,30 +38,30 @@ void Manager::print_parameters()
 
 void Manager::update()
 {
-    STATES state = state_machine.get_state();
-    switch (state)
-    {
-    case STATES::STOPPED:
-        STOPPED_action();
-        break;
-    case STATES::TAKE_OFF:
-        TAKE_OFF_action();
-        break;
-    case STATES::LAND:
-        LAND_action();
-        break;
-    case STATES::JOY_CONTROL:
-        JOY_CONTROL_action();
-        break;
-    case STATES::LAND_CONTROL:
-        LAND_CONTROL_action();
-        break;
-    case STATES::FOLLOW_CONTROL:
-        FOLLOW_CONTROL_action();
-        break;
-    default:
-        break;
-    }
+    // STATES state = state_machine.get_state();
+    // switch (state)
+    // {
+    // case STATES::STOPPED:
+    //     STOPPED_action();
+    //     break;
+    // case STATES::TAKE_OFF:
+    //     TAKE_OFF_action();
+    //     break;
+    // case STATES::LAND:
+    //     LAND_action();
+    //     break;
+    // case STATES::JOY_CONTROL:
+    //     JOY_CONTROL_action();
+    //     break;
+    // case STATES::LAND_CONTROL:
+    //     LAND_CONTROL_action();
+    //     break;
+    // case STATES::FOLLOW_CONTROL:
+    //     FOLLOW_CONTROL_action();
+    //     break;
+    // default:
+    //     break;
+    // }
 
     follow_controller.update_parameters(parameters);
     land_controller.update_parameters(parameters);
@@ -80,14 +80,14 @@ void Manager::STOPPED_action()
 
 void Manager::TAKE_OFF_action()
 {
-    std_msgs::Empty emptyMsg;
-    ROS_client->takeoff_pub.publish(emptyMsg);
+    // std_msgs::Empty emptyMsg;
+    // ROS_client->takeoff_pub.publish(emptyMsg);
 }
 
 void Manager::LAND_action()
 {
-    std_msgs::Empty emptyMsg;
-    ROS_client->land_pub.publish(emptyMsg);
+    // std_msgs::Empty emptyMsg;
+    // ROS_client->land_pub.publish(emptyMsg);
 }
 
 void Manager::JOY_CONTROL_action()
@@ -164,9 +164,9 @@ void Manager::LAND_CONTROL_action()
     cout << "completed_approach: " << land_controller.completed_approach() << endl;
     if (land_controller.completed_approach())
     {
-        std_msgs::Empty emptyMsg;
-        ROS_client->land_pub.publish(emptyMsg);
-        state_machine.land();
+        // std_msgs::Empty emptyMsg;
+        // ROS_client->land_pub.publish(emptyMsg);
+        // state_machine.land();
     }
     cout << "**********************" << endl;
 }
@@ -189,20 +189,20 @@ void Manager::FOLLOW_CONTROL_action()
 
 void Manager::send_velocity(double x_linear, double y_linear, double z_linear, double angular)
 {
-    cout << "x_linear: " << x_linear
-         << "\ty_linear: " << y_linear
-         << "\tz_linear: " << z_linear
-         << "\tangular: " << angular << endl;
+    // cout << "x_linear: " << x_linear
+    //      << "\ty_linear: " << y_linear
+    //      << "\tz_linear: " << z_linear
+    //      << "\tangular: " << angular << endl;
 
-    geometry_msgs::Twist velocity;
-    velocity.linear.x = -y_linear;
-    velocity.linear.y = x_linear;
-    velocity.linear.z = z_linear;
+    // geometry_msgs::Twist velocity;
+    // velocity.linear.x = -y_linear;
+    // velocity.linear.y = x_linear;
+    // velocity.linear.z = z_linear;
 
-    velocity.angular.x = 0;
-    velocity.angular.y = 0;
-    velocity.angular.z = -angular;
-    ROS_client->cmd_vel_pub.publish(velocity);
+    // velocity.angular.x = 0;
+    // velocity.angular.y = 0;
+    // velocity.angular.z = -angular;
+    // ROS_client->cmd_vel_pub.publish(velocity);
 }
 
 void Manager::poseCallback(const geometry_msgs::PoseStamped::ConstPtr &msg)
