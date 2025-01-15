a800fd0c49f750dd1a599b353e5581764bdb4553
luciano <luciano.bj23@hotmail.com>
2025-01-14 22:36:03 -0300

    aruco detection

--- iris_land/scripts/aruco_node.py
+++ iris_land/scripts/aruco_node.py
@@ -77,95 +77,100 @@ class ImageReader:
     
     def image_callback(self, msg):
         image = self.bridge.imgmsg_to_cv2(msg, desired_encoding='passthrough')
+        pose_msg, image = self.position_detect(image)
 
-        republished_msg = self.bridge.cv2_to_imgmsg(image, encoding='bgr8')
-        self.image_pub.publish(republished_msg)
+        # image message creation
+        image_msg = self.bridge.cv2_to_imgmsg(image, encoding='bgr8')
+        self.image_pub.publish(image_msg)
 
+        # pose message creation
+        if pose_msg is not None:
+            pose_msg.header.stamp = rospy.Time.now()
+            pose_msg.header.frame_id = "camera_frame"
+            self.pose_pub.publish(pose_msg)
 
-    # def image_callback(self, msg):
-    #     image = self.bridge.imgmsg_to_cv2(msg, desired_encoding='passthrough')
-    #     # pose_msg, image = self.position_detect(image)
+    def position_detect(self, image):
+        pose_msg = None
+        gray_frame = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        corners, ids, _ = aruco.detectMarkers(gray_frame, self.dictionary, parameters=self.parameters)
+        return_image = aruco.drawDetectedMarkers(image, corners, ids)
 
-    #     self.image_pub.publish(image)
-    #     # self.pose_pub.publish(pose_msg)
+        if ids is not None:
+            ids_to_process = [
+                (i, id[0])
+                for i, id in enumerate(ids)
+                if id[0] in self.marker_sizes
+            ]
 
-    # def position_detect(self, image):
-    #     gray_frame = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    #     # corners, ids, _ = aruco.detectMarkers(gray_frame, self.dictionary, parameters=self.parameters)
-    #     # return_img = aruco.drawDetectedMarkers(image, corners, ids)
-    #     return_img = gray_frame
-        
-    #     # print(len(corners))
+            positions = []
+            orientations = []
+            for i, marker_id in ids_to_process:
+                marker_length = self.marker_sizes[marker_id]
+                rvecs, tvecs, _objPoints = aruco.estimatePoseSingleMarkers(
+                    corners[i : i + 1],
+                    marker_length,
+                    self.camera_matrix,
+                    self.distortion_coeffs,
+                )
 
-    #     # if ids is not None:
-    #     #     ids_to_process = [
-    #     #         (i, id[0])
-    #     #         for i, id in enumerate(ids)
-    #     #         if id[0] in self.marker_sizes
-    #     #     ]
-
-    #     #     for i, marker_id in ids_to_process:
-    #     #         marker_length = self.marker_sizes[marker_id]
-
-    #     #         rvecs, tvecs, _objPoints = aruco.estimatePoseSingleMarkers(
-    #     #                 corners[i : i + 1],
-    #     #                 marker_length,
-    #     #                 self.camera_matrix,
-    #     #                 self.distortion_coeffs,
-    #     #             )
-
-    #     #         #############
-    #     #         # frame = cv2.drawFrameAxes(frame,self.camera_matrix,self.distortion_coeffs,rvecs,tvecs,marker_length)
-    #     #         #############
+                return_image = cv2.drawFrameAxes(return_image,self.camera_matrix,self.distortion_coeffs,rvecs,tvecs,marker_length)
             
-    #     #         if rvecs is not None and tvecs is not None:
-    #     #             tvecs = np.squeeze(tvecs)
-    #     #             rvecs = np.squeeze(rvecs)
+                if rvecs is not None and tvecs is not None:
+                    tvecs = np.squeeze(tvecs)
+                    rvecs = np.squeeze(rvecs)
+                    pos_landpad_to_camera, rot_landpad_to_camera = self._landpad_to_camera(tvecs, rvecs, marker_id)
 
-    #     #             pos_landpad_to_camera, rot_landpad_to_camera = self._landpad_to_camera(tvecs, rvecs, marker_id)
+                    # Armazena os valores na lista
+                    if pos_landpad_to_camera is not None and rot_landpad_to_camera is not None:
+                        positions.append(pos_landpad_to_camera)
+                        orientations.append(rot_landpad_to_camera)
+                    
+            if positions and orientations:
+                avg_position = np.mean(positions, axis=0)
+                avg_orientation = np.mean(orientations, axis=0)
 
+                # Converte os ângulos médios de Euler para quaternion
+                quaternion = R.from_euler('ZYX', avg_orientation, degrees=True).as_quat()
 
-    #     #         pose_msg = PoseStamped()
-    #     #         pose_msg.header.stamp = rospy.Time.now()
-    #     #         # pose_msg.header.frame_id = "camera_frame"
+                # Preenche o pose_msg com os valores médios
+                pose_msg = PoseStamped()
+                pose_msg.pose.position.x = avg_position[0]
+                pose_msg.pose.position.y = avg_position[1]
+                pose_msg.pose.position.z = avg_position[2]
 
-    #     #         pose_msg.pose.position.x = -tvec[1]
-    #     #         pose_msg.pose.position.y = -tvec[0]
-    #     #         pose_msg.pose.position.z = -tvec[2]
+                pose_msg.pose.orientation.x = quaternion[0]
+                pose_msg.pose.orientation.y = quaternion[1]
+                pose_msg.pose.orientation.z = quaternion[2]
+                pose_msg.pose.orientation.w = quaternion[3]
 
-    #     #         pose_msg.pose.orientation.x = rotation_matrix_euler[0]
-    #     #         pose_msg.pose.orientation.y = rotation_matrix_euler[1]
-    #     #         pose_msg.pose.orientation.z = rotation_matrix_euler[2]
+        return pose_msg, return_image
 
-    #     return 0, return_img
+    def _landpad_to_camera(self, Tvec, Rvec, id):
+        if id not in [272, 682, 0]:
+            return None, None
 
-    # def _landpad_to_camera(self, Tvec, Rvec, id):
-    #     if id not in [272, 682, 0]:
-    #         return np.array([0, 0, 0])
-    #         # return np.array([-999.0, -999.0, -999.0])
+        # Cria a matriz de transformação Aruco -> Câmera
+        r = R.from_rotvec(Rvec)
+        TM_Aruco_To_Camera = np.eye(4)
+        TM_Aruco_To_Camera[:3, :3] = r.as_matrix()
+        TM_Aruco_To_Camera[:3, 3] = Tvec
 
-    #     # Cria a matriz de transformação Aruco -> Câmera
-    #     r = R.from_rotvec(Rvec)
-    #     TM_Aruco_To_Camera = np.eye(4)
-    #     TM_Aruco_To_Camera[:3, :3] = r.as_matrix()
-    #     TM_Aruco_To_Camera[:3, 3] = Tvec
+        # Cria a matriz de transformação Landpad -> Câmera
+        TM_Landpad_To_Camera = TM_Aruco_To_Camera @ self.TM_Landpad_To_Aruco_000
+        if id == 272:
+            TM_Landpad_To_Camera = TM_Aruco_To_Camera @ self.TM_Landpad_To_Aruco_272
+        elif id == 682:
+            TM_Landpad_To_Camera = TM_Aruco_To_Camera @ self.TM_Landpad_To_Aruco_682
+        elif id == 0:
+            TM_Landpad_To_Camera = TM_Aruco_To_Camera @ self.TM_Landpad_To_Aruco_000
 
-    #     # Cria a matriz de transformação Landpad -> Câmera
-    #     TM_Landpad_To_Camera = TM_Aruco_To_Camera @ self.TM_Landpad_To_Aruco_000
-    #     if id == 272:
-    #         TM_Landpad_To_Camera = TM_Aruco_To_Camera @ self.TM_Landpad_To_Aruco_272
-    #     elif id == 682:
-    #         TM_Landpad_To_Camera = TM_Aruco_To_Camera @ self.TM_Landpad_To_Aruco_682
-    #     elif id == 0:
-    #         TM_Landpad_To_Camera = TM_Aruco_To_Camera @ self.TM_Landpad_To_Aruco_000
+        pos_landpad_to_camera = TM_Landpad_To_Camera[:3, 3]
+        pos_landpad_to_camera[1] = -pos_landpad_to_camera[1]
 
-    #     pos_landpad_to_camera = TM_Landpad_To_Camera[:3, 3]
-    #     pos_landpad_to_camera[1] = -pos_landpad_to_camera[1]
+        rotation_landpad_to_camera = R.from_matrix(TM_Landpad_To_Camera[:3, :3])
+        rot_landpad_to_camera = rotation_landpad_to_camera.as_euler('ZYX', degrees=True) # roll, pitch, yaw
 
-    #     rotation_landpad_to_camera = R.from_matrix(TM_Landpad_To_Camera[:3, :3])
-    #     rot_landpad_to_camera = rotation_landpad_to_camera.as_euler('ZYX', degrees=True) # roll, pitch, yaw
-
-    #     return pos_landpad_to_camera, rot_landpad_to_camera
+        return pos_landpad_to_camera, rot_landpad_to_camera
 
     def run(self):
         rospy.spin()
