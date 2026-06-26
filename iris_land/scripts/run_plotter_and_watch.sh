e5d3fa7155b31e6f75fc91d9a67717a24f4d1410
jbneto1 <jabraunneto1@gmail.com>
2026-06-26 11:39:24 +0100

    removed sh

--- iris_land/scripts/run_plotter_and_watch.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-roslaunch drone_ekf plotter.launch
-
-echo ""
-echo "Plotter launch exited."
-echo "Watching detached saver log. Press Ctrl+C to stop watching."
-echo ""
-
-tail -f /tmp/ekf_plots/plotter_shutdown_save.log
